58e7bf9881a2ce0398a00da2c0c55a4d2a028b4f
Sirbucket <51178809+Sirbucket@users.noreply.github.com>
2023-08-26 23:00:57 -0400

    begin

---
+++ entities/weapons/weapon_cbt_base.lua
@@ -0,0 +1,8 @@
+SWEP.PrintName = "Base Weapon"
+SWEP.Description = "Gamemode Base Weapon, how tf did you get this"
+
+SWEP.MeleeDamage = 200
+SWEP.MeleeKnockback = 200
+SWEP.MeleeRange = 200
+SWEP.MeleeSwingDelay = 0.25
+SWEP.MeleeSwingSpeed = 1
